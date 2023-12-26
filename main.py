cd166aa8a53797a99a8671b7a7b3227669f4f216
Andor Szabó <szandor.92@gmail.com>
2023-12-26 12:25:50 +0200

    Created main module

---
+++ main.py
@@ -0,0 +1,8 @@
+from capital import Capital
+from city import City
+
+city = City("Székelyudvarhely", 34000)
+print(city)
+
+capital = Capital(name="Bukarest", population=2000000, contamination=138, nr_of_metros=6)
+print(capital)
